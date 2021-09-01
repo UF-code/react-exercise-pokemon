59e974345f0c2cc66135b74eb9cc4582ce23de70
uf-code <ugur.firat.developer@gmail.com>
2021-09-01 22:19:15 +0300

    adding logic to decides who is the winner

--- src/Pokedex.jsx
+++ src/Pokedex.jsx
@@ -5,9 +5,13 @@ export class Pokedex extends Component {
     render() {
         return (
             <div className='Pokedex'>
-                {this.props.pokemon.map((p) => (
-                    <Pokecard id={p.id} name={p.name} type={p.type} exp={p.base_experience} />
-                ))}
+                <p>Total Experience: {this.props.exp} </p>
+                <p> {this.props.isWinner ? 'Winner' : 'Loser'} </p>
+                <div className='Pokedex-cards'>
+                    {this.props.pokemon.map((p) => (
+                        <Pokecard id={p.id} name={p.name} type={p.type} exp={p.base_experience} />
+                    ))}
+                </div>
             </div>
         )
     }
