337b1e42cc0ea7a31bf12a42d9abfeda92e0e932
uf-code <ugur.firat.developer@gmail.com>
2021-09-01 22:37:55 +0300

    adding more style and logic for winning and losing hands

--- src/Pokedex.jsx
+++ src/Pokedex.jsx
@@ -4,10 +4,16 @@ import './Pokedex.css'
 
 export class Pokedex extends Component {
     render() {
+        let title
+        if (this.props.isWinner) {
+            title = <h1 className='Pokedex-winner'>Winning Hand</h1>
+        } else {
+            title = <h1 className='Pokedex-loser'>Losing Hand</h1>
+        }
         return (
             <div className='Pokedex'>
-                <p>Total Experience: {this.props.exp} </p>
-                <p> {this.props.isWinner ? 'Winner' : 'Loser'} </p>
+                {title}
+                <h4>Total Experience: {this.props.exp} </h4>
                 <div className='Pokedex-cards'>
                     {this.props.pokemon.map((p) => (
                         <Pokecard id={p.id} name={p.name} type={p.type} exp={p.base_experience} />
