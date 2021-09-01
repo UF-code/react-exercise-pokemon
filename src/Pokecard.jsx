85820f63aa06e41f96cbce92a7dabad6a7a68b3e
uf-code <ugur.firat.developer@gmail.com>
2021-09-01 22:31:05 +0300

    adding some styles

--- src/Pokecard.jsx
+++ src/Pokecard.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import './Pokecard.css'
+
 const POKE_API = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/'
 
 let padToThree = (number) => (number <= 999 ? `00${number}`.slice(-3) : number)
@@ -9,10 +11,10 @@ export class Pokecard extends Component {
         console.log(imgSrc)
         return (
             <div className='Pokecard'>
-                <h1> {this.props.name} </h1>
+                <h1 className='Pokecard-title'> {this.props.name} </h1>
                 <img src={imgSrc} alt={this.props.name} />
-                <div className='Type'> Type: {this.props.type} </div>
-                <div className='Exp'> Exp: {this.props.exp} </div>
+                <div className='Pokecard-data'> Type: {this.props.type} </div>
+                <div className='Pokecard-data'> Exp: {this.props.exp} </div>
             </div>
         )
     }
--- src/Pokedex.jsx
+++ src/Pokedex.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Pokecard from './Pokecard'
+import './Pokedex.css'
 
 export class Pokedex extends Component {
     render() {
